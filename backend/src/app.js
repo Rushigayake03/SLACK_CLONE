50a3852a1ac661074c8595b7171b706a7905314f
Rushigayake03 <119030444+Rushigayake03@users.noreply.github.com>
2025-11-02 19:32:52 +0530

    auth implemented

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import {clerkMiddleware} from "@clerk/express";
+//import {clerkClient} from "@clerk/clerk-sdk-node";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 // 
 
 const app = express();
 
+app.use(express.json());
+app.use(clerkMiddleware());//req.auth will be available in the routes after this line
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/" , (req, res) => {
     res.send("Hello World! 123");
@@ -12,7 +20,20 @@ app.get("/" , (req, res) => {
 
 //console.log("mongo uri:", ENV.MONGO_URI);
 
-app.listen(ENV.PORT, () => {
-    console.log("server started on port:",ENV.PORT)
-    connectDB();
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+        if(ENV.NODE_ENV === "production"){
+            app.listen(ENV.PORT, () => {
+                console.log("server started on port:",ENV.PORT)
+        });
+        }
+        } catch (error) {
+            console.error("Error starting server:", error);
+            process.exit(1);
+        }
+};
+
+startServer();
+
+export default app;
